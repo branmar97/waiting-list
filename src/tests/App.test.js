40e93fef68ca2f28771172370ebb7f621627472f
branmar97 <brandonmar97@gmail.com>
2021-05-15 10:35:58 -0400

    add app render test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import App from '../App';
 import { shallow, mount } from 'enzyme';
 import toJson from "enzyme-to-json";
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+});
