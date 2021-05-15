ccdb3d93f5636e46c67ea296588f2b0a4e31df5e
branmar97 <brandonmar97@gmail.com>
2021-05-15 09:42:31 -0400

    create waiting list state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import React, { useState } from 'react';
 import Input from './components/Input';
 import List from './components/List';
 
 function App() {
+  const [waitingList, setWaitingList] = useState([])
+
   return (
     <div className="App">
       <Input />
