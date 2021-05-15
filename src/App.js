708c9f247d5d62adde2ed6266871b63436337ca2
branmar97 <brandonmar97@gmail.com>
2021-05-15 10:17:10 -0400

    create remove from list function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import List from './components/List';
 function App() {
   const [waitingList, setWaitingList] = useState([])
 
+  const removeFromList = (name) => {
+    const newList = waitingList.filter(person => person != name)
+    setWaitingList(newList)
+  } 
+
   return (
     <div className="App">
       <Input list={waitingList} updateList={setWaitingList} />
       <h1>People Waiting</h1>
-      <List waiting={waitingList} />
+      <List waiting={waitingList} removeFromList={removeFromList} />
     </div>
   );
 }
