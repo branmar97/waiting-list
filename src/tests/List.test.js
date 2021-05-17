b0f1c18fedcff6ba24d6a4ae3c24ddde273b101d
branmar97 <brandonmar97@gmail.com>
2021-05-17 09:36:02 -0400

    Setup initial tests for list component

--- src/tests/List.test.js
+++ src/tests/List.test.js
@@ -1,4 +1,12 @@
 import React from 'react';
-import App from '../App';
+import List from '../components/List';
 import { shallow, mount } from 'enzyme';
 import toJson from "enzyme-to-json";
+
+const names = ['Wilma', 'Emily', 'Carter'];
+
+describe("rendering components", () => {
+    it("renders List component without crashing", () => {
+      shallow(<List waiting={names} />);
+    });
+  });
