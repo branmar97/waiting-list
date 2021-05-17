c3f4fc418831f24e77b45acee68fe539563c878d
branmar97 <brandonmar97@gmail.com>
2021-05-17 14:22:16 -0400

    add test for input form

--- src/tests/Input.test.js
+++ src/tests/Input.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Input from '../components/Input';
-import { render ,fireEvent } from "@testing-library/react"
+import { render , fireEvent } from "@testing-library/react"
 
 describe("rendering components", () => {
     it("renders Input component without crashing", () => {
       render(<Input />);
+    });
+    it("contains Input form", () => {
+        const { queryByTestId } = render(<Input />)
+
+        expect(queryByTestId("input-form")).toBeTruthy()
     });
   });
